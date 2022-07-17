db7934a3f4e4b20de0a5102045b743d35d6ef58e
Mohammad Sohrab Hossain <sohrab.cse9@gmail.com>
2022-07-17 16:05:01 +0600

    front end api change

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const server = app.listen(process.env.PORT || PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://nassachatapp.netlify.app/",
     credentials: true,
   },
 });
